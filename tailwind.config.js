28e96a06a84400a634e64f4748b7067716dbe609
Jason Yuan <jason.yuan869@gmail.com>
2024-07-03 16:33:52 -0400

    chore: formatting with prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import * as daisyui from 'daisyui'
+import * as daisyui from 'daisyui';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
@@ -8,12 +8,11 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Poppins', ...defaultTheme.fontFamily.sans]
-      },
-    },
+      }
+    }
   },
   daisyui: {
     themes: ['cupcake', 'dracula']
   },
-  plugins: [daisyui],
-}
-
+  plugins: [daisyui]
+};
